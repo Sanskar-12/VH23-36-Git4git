6ffb02776d361ad3203ff5d2621dbfc7b24e2336
Ayush <ayushsingh00030@gmail.com>
2023-10-07 01:05:54 +0530

    About Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-// import Navbar from './components/Navbar';
-// import Footer from './components/Footer';
+
 import Navbar from "./Components/Navbar/navbar";
+import About from "./Components/About/About"
 import Hero from "./Components/Hero/Hero";
 import Footer from "./Components/Footer/Footer"
+import Services from "./Components/Services/services"
+import Help from "./Components/Help/help"
+import Technology from "./Components/Technology/technology"
+import Login from "./Components/Login/login"
+import Register from "./Components/Register/register"
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
 
 
 const App = () => {
   return (
     <>
+    <Router>
       <Navbar />
-      <Hero />
+      <Routes>
+        <Route path="/home" element={<Hero/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/services" element={<Services/>}/>
+        <Route path="/help" element={<Help/>}/>
+        <Route path="/technology" element={<Technology/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+
       <Footer />
+    </Router>
+      
     </>
   );
 }
--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,89 +1,118 @@
-
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 const navbar = () => {
   return (
     <>
-    <nav className="bg-gradient-to-r from-blue-600 via-blue-400 to-blue-200 ...">
+      <nav className="bg-gradient-to-r from-blue-600 via-blue-400 to-blue-200 ...">
         <div className="relative w-[1080px] mx-auto flex items-center justify-between ">
+          <Link to="/home" className="cursor-pointer py-7 pr-7 flex space-x-4">
+            <img
+              src="./src/Assets/images/blockchainlogo.jpg"
+              width="40px"
+              height="20px"
+            />
+            <p className="text-white font-bold mt-2 text-lg">Git4Git</p>
+          </Link>
 
-            
-            <a href="/" className="cursor-pointer py-7 pr-7 flex space-x-4">
-                <img
-                src="./src/Assets/images/blockchainlogo.jpg"
-                width="40px"
-                height="20px"/>
-                <p className="text-white font-bold mt-2 text-lg">Git4Git</p>
-            </a>
+          <ul className="flex space-x-6">
+            <li
+              className="text-white font-mullish text-lg py-7 hover:text-pink cursor-pointer
+                 transition-all duration-200 relative group"
+            >
+              <Link to="/home">Home</Link>
 
-            <ul className="flex space-x-6">
-            <li className="text-white font-mullish text-lg py-7 hover:text-pink cursor-pointer
-                 transition-all duration-200 relative group">
-                <a href="#">Home</a>
+              <div
+                className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block
+                transition-all duration-200"
+              ></div>
+            </li>
+            <li
+              className="text-white font-mullish text-lg py-7 hover:text-pink cursor-pointer
+                 transition-all duration-200 relative group"
+            >
+              <Link to="/about">About Us</Link>
 
-                <div className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block
-                transition-all duration-200" ></div>
-                </li>
-                <li className="text-white font-mullish text-lg py-7 hover:text-pink cursor-pointer
-                 transition-all duration-200 relative group">
-                <a href="#">About Us</a>
+              <div
+                className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block
+                transition-all duration-200"
+              ></div>
+            </li>
+            <li
+              className="text-white font-mullish text-lg py-7 hover:text-pink cursor-pointer
+                 transition-all duration-200 relative group"
+            >
+              <Link to="/technology">Technology</Link>
 
-                <div className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block
-                transition-all duration-200" ></div>
-                </li>
-                <li className="text-white font-mullish text-lg py-7 hover:text-pink cursor-pointer
-                 transition-all duration-200 relative group">
-                <a href="#">Technology</a>
+              <div
+                className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block
+                transition-all duration-200"
+              ></div>
+            </li>
 
-                <div className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block
-                transition-all duration-200" ></div>
-                </li>
+            <li
+              className="text-white font-mullish text-lg py-7 hover:text-pink cursor-pointer
+                 transition-all duration-200 relative group"
+            >
+              <Link to="services">Services</Link>
 
-                <li className="text-white font-mullish text-lg py-7 hover:text-pink cursor-pointer
-                 transition-all duration-200 relative group">
-                <a href="#">Services</a>
+              <div
+                className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block
+                transition-all duration-200"
+              ></div>
+            </li>
 
-                <div className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block
-                transition-all duration-200" ></div>
-                </li>
-                
-                <li className="text-white font-mullish text-lg py-7 hover:text-pink cursor-pointer
-                 transition-all duration-200 relative group">
-                <a href="#">Help</a>
+            <li
+              className="text-white font-mullish text-lg py-7 hover:text-pink cursor-pointer
+                 transition-all duration-200 relative group"
+            >
+              <Link to="/help">Help</Link>
 
-                <div className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block
-                transition-all duration-200" ></div>
-                </li>
-            </ul>
+              <div
+                className="absolute bottom-0 w-full h-1 bg-lightBlue hidden group-hover:block
+                transition-all duration-200"
+              ></div>
+            </li>
+          </ul>
 
-            <div className="flex space-x-6">
-                <img
+          <div className="flex space-x-6">
+            <img
+              src="./src/Assets/images/india-flag.svg"
+              width="28px"
+              height="20px"
+            />
+            <div className="mt-2 text-2xl">
+              <a href="https://github.com/Sanskar-12/VH23-36-Git4git">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </div>
+            <Link to={"/login"}>
+              <button
+                className="py-3 px-5 font-mullish text-md text-white border-lightBlue hover:bg-white hover:text-deepBlue
+                border rounded-sm  font-bold"
+              >
+                Log in
+              </button>
+            </Link>
 
-                    src="./src/Assets/images/india-flag.svg"
-                    width="28px"
-                    height="20px"
-                    />
-                   <div className='mt-2 text-2xl'>
-                   <a href="https://github.com/Sanskar-12/VH23-36-Git4git"><FontAwesomeIcon icon={faGithub} /></a>
-                   </div>
-
-                <button className="py-3 px-5 font-mullish text-md text-white border-lightBlue hover:bg-wheat hover:text-deepBlue
-                border rounded-sm  font-bold">Log in</button>
-                <button className="py-3 px-4 font-mullish rounded-sm text-md font-bold
+            <Link to={"/register"}>
+              <button
+                className="py-3 px-4 font-mullish rounded-sm text-md font-bold
 
                 bg-white text-lightBlue300 border transition-all duration-200
-                hover:bg-deepBlue hover:text-wheat flex">Sign Up
-                </button>
-            </div>
-
-
+                hover:bg-deepBlue hover:text-white flex"
+              >
+                Sign Up
+              </button>
+            </Link>
+          </div>
         </div>
-    </nav>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default navbar
+export default navbar;

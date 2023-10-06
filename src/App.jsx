e6b3fb1f8afe4b5f8af131a5360d3d38049ed30d
Prasad Vavdane <prasadvavdane45@gmail.com>
2023-10-06 13:15:51 +0530

    added components folders

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Footer />
+    </>
+  );
+}

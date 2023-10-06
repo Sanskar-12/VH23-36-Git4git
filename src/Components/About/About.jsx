344d8be150afecd462f2647aea6c4b6f03e3e141
Prasad Vavdane <prasadvavdane45@gmail.com>
2023-10-07 01:44:41 +0530

    ignore

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,48 +1,23 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Footer from "../Footer/Footer";
-import Navbar from "../Navbar/navbar";
-
-function About() {
-  return (
-    <div>
-      <Footer/>
-      <Navbar/>
-    </div>
-  )
-}
-
-export default About
-=======
-=======
-
->>>>>>> eb3536f1de1af371f62ee31ec3ca41b2a52a1ea0
-import React from 'react'
-import Testimonial from '../Testimonial/testimonial'
+import Testimonial from "../Testimonial/testimonial";
 const About = () => {
   return (
     <>
-    <div className='font-mullish flex flex-col justify-between items-center my-12 text-gray-800'>
-        <h2 className='text-4xl'>The Team Behind Git4Git</h2>
-<p className='line-clamp-2 flex flex-col'><span>There are many variation of passages of Lorem Ipsum available but the</span>
-<span className='px-8'>majority have suffered alteration in some injected humour</span></p>  
+      <div className="font-mullish flex flex-col justify-between items-center my-12 text-gray-800">
+        <h2 className="text-4xl">The Team Behind Git4Git</h2>
+        <p className="line-clamp-2 flex flex-col">
+          <span>
+            There are many variation of passages of Lorem Ipsum available but
+            the
+          </span>
+          <span className="px-8">
+            majority have suffered alteration in some injected humour
+          </span>
+        </p>
 
-<Testimonial />  
-
-
-
-
-
-   
-
-    </div>
+        <Testimonial />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default About
-<<<<<<< HEAD
->>>>>>> 6df1bf2c4dd7f4a1724634a19ce2fca344ded7b5
-=======
-
->>>>>>> eb3536f1de1af371f62ee31ec3ca41b2a52a1ea0
+export default About;

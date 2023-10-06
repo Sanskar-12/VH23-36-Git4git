d4cb1196e1adbd70c2058fd6c2a9b07b462a06f5
Prasad Vavdane <prasadvavdane45@gmail.com>
2023-10-07 01:09:36 +0530

    navbar and about

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,25 @@
 import Navbar from "./Components/Navbar/navbar";
 import Hero from "./Components/Hero/Hero";
 import Footer from "./Components/Footer/Footer"
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
 
 
 const App = () => {
   return (
     <>
+    <Router>
       <Navbar />
+      <Routes>
+        <Route path="/Home" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+
+    </Router>
+      
       <Hero />
       <Footer />
     </>
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,13 @@
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/navbar";
+
+function About() {
+  return (
+    <div>
+      <Footer/>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default About

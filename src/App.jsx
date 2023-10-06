918f56909f5f361993c5a11d99e9adcb7d864c57
Ayush <ayushsingh00030@gmail.com>
2023-10-06 18:34:16 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import Footer from "./Components/Footer/Footer"
 const App = () => {
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route ></Route>
+    </Routes>
+    </BrowserRouter>
       <Navbar />
       <Hero />
       <Footer />

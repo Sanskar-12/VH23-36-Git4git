d6c487f1f0d6d750e3c9d751e55db9e2c984d6f1
Sanskar-12 <sanskarv2004@gmail.com>
2023-10-07 07:25:33 +0530

    backend config

---
+++ middlewares/isAuthenticated.js
@@ -0,0 +1,29 @@
+import { User } from "../models/User.js"
+import jwt from "jsonwebtoken"
+
+const isAuthenticated=async(req,res,next)=>{
+    try {
+        
+        const {token}=req.cookies
+        if(!token)
+        {
+            return res.status(400).json({
+                success:false,
+                message:"Login First!"
+            })
+        }
+    
+        const decoded=jwt.verify(token,process.env.JWT_SECRET)
+    
+        req.user=await User.findById(decoded._id)
+    
+        next()
+    } catch (error) {
+        res.status(400).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+export default isAuthenticated
